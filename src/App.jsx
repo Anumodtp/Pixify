eaa92429ffb6887f64583ba67eb56124c1b50d1c
Anumod <anumodtp555@gmail.com>
2024-08-20 22:39:23 +0530

    index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="root">
+        <h1>Pixifyy</h1>
+        <p id="description">Pixifyy is amazing.</p>
+        <button class="explore-button">Explore</button>
+        <div class="auth-links">
+          <a href="#" class="auth-link">
+            SignUp
+          </a>
+          or
+          <a href="#" class="auth-link">
+            LogIn
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
