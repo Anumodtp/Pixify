3dd76781698daeb3f39035481143189b5ee5bf1a
Anumod <anumodtp555@gmail.com>
2024-08-21 20:42:18 +0530

    feedpage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
-// src/App.jsx
 import React, { useState } from "react";
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  useNavigate,
+} from "react-router-dom";
 import SignUp from "./SignUp";
 import LogIn from "./LogIn";
+import FeedPage from "./FeedPage";
 import "./App.css";
 
 function App() {
-  const [view, setView] = useState("home"); // "home", "login", "signup"
+  const [view, setView] = useState("home");
+  const navigate = useNavigate();
+
+  const handleExploreClick = () => {
+    navigate("/feed"); //feed page
+  };
 
   return (
     <div id="root">
-      {view === "home" && (
-        <>
-          <h1>Pixifyy</h1>
-          <p id="description">Pixifyy is amazing.</p>
-          <button className="explore-button">Explore</button>
-          <div className="auth-links">
-            <a href="#" className="auth-link" onClick={() => setView("signup")}>
-              SignUp
-            </a>
-            or
-            <a href="#" className="auth-link" onClick={() => setView("login")}>
-              LogIn
-            </a>
-          </div>
-        </>
-      )}
-
-      {view === "signup" && <SignUp onLoginClick={() => setView("login")} />}
-      {view === "login" && <LogIn onSignUpClick={() => setView("signup")} />}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            view === "home" ? (
+              <>
+                <h1>Pixifyy</h1>
+                <p id="description">Pixifyy is amazing.</p>
+                <button className="explore-button" onClick={handleExploreClick}>
+                  Explore
+                </button>
+                <div className="auth-links">
+                  <button
+                    className="auth-link"
+                    onClick={() => setView("signup")}
+                  >
+                    SignUp
+                  </button>
+                  or
+                  <button
+                    className="auth-link"
+                    onClick={() => setView("login")}
+                  >
+                    LogIn
+                  </button>
+                </div>
+              </>
+            ) : view === "signup" ? (
+              <SignUp onLoginClick={() => setView("login")} />
+            ) : view === "login" ? (
+              <LogIn onSignUpClick={() => setView("signup")} />
+            ) : null
+          }
+        />
+        <Route path="/feed" element={<FeedPage />} />
+        {/*ivde routes add akkane*/}
+      </Routes>
     </div>
   );
 }
---
+++ src/FeedPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./Feedpage.css";
+
+function FeedPage() {
+  return (
+    <div className="feedpage">
+      <h1>Welcome to the Feed Page</h1>
+    </div>
+  );
+}
+
+export default FeedPage;
